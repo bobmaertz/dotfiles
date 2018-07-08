b4ab64cf70843f2d5780a80b20b31d4a1c1f2e48
Bob Maertz <r.maertz@gmail.com>
2018-07-08 12:36:13 -0500

    Added instalation and removed settings that caused bug with s key.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Get this directory 
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#Set Symlinks
+ln -sfv "$DOTFILES_DIR/common/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+
+
