7d80da3f9f0b731028d439c93c02a059c756612a
Bob Maertz <r.maertz@gmail.com>
2019-06-08 22:01:31 -0400

    Updated installation script.

--- install.sh
+++ install.sh
@@ -7,5 +7,12 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Set Symlinks
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-
+ln -sfv "$DOTFILES_DIR/bash/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/bash/.bash_aliases" ~
+ln -sfv "$DOTFILES_DIR/bash/.env" ~
+ln -sfv "$DOTFILES_DIR/bash/.functions" ~
+ln -sfv "$DOTFILES_DIR/bash/.grep" ~
+ln -sfv "$DOTFILES_DIR/bash/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/bash/.profile" ~
+ln -sfv "$DOTFILES_DIR/bash/.viminfo" ~
 
